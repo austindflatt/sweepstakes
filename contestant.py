af572c33ee4a82f256f05f51a680c168c6a1d1ad
Austin Flatt <austindflatt@gmail.com>
2021-05-27 10:55:39 -0400

    redid variables to contestant class, the init method of Contestant class will prompt user for attribute values. Registration number is randomly generated

--- contestant.py
+++ contestant.py
@@ -1,9 +1,20 @@
+import random
+
+
 class Contestant:
-    def __init__(self, first_name, last_name, email, registration_number):
-        self.contestant = {first_name, last_name, email, registration_number}
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.registration_number = registration_number
-    def is_winner(self):
-        pass
+    def __init__(self):
+        self.first_name = str
+        self.last_name = str
+        self.email_address = str
+        self.registration_number = int
+
+        self.first_name = input("\nFirst Name: ")
+        self.last_name = input("Last Name: ")
+        self.email_address = input("Email Address: ")
+        rand = random.Random(1, 100)
+        self.registration_number = rand
+        print(
+            f"contestant info:\n\tfirst name: {self.first_name}\n\tlast name: {self.last_name}\n\temail address: {self.email_address}\n\tregistration number: {self.registration_number}")
+
+    def notify(self, is_winner):
+        print(f"Congratulations {is_winner.first_name} {is_winner.last_name}!")
